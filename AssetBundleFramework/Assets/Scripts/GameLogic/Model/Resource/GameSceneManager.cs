2eccd8cb3d3c4ce3b3664f08f8be22d2dfa641d5
TonyTang <huantang0529@gmail.com>
2026-07-25 22:48:57 +0800

    Feat：1. 提供上层特定资源加载计数释放和请求取消的统一工具类(ResourceScope)，方便上层逻辑实现特定上下文的资源加载计数释放和请求取消。

--- AssetBundleFramework/Assets/Scripts/GameLogic/Model/Resource/GameSceneManager.cs
+++ AssetBundleFramework/Assets/Scripts/GameLogic/Model/Resource/GameSceneManager.cs
@@ -19,9 +19,19 @@ using UnityEngine.SceneManagement;
 public class GameSceneManager : SingletonTemplate<GameSceneManager>
 {
     /// <summary>
-    /// 当前场景的Asset加载器信息
+    /// 当前场景AB路径
     /// </summary>
-    private BundleLoader mCurrentSceneBundleLoader;
+    private string mCurrentSceneABPath;
+    
+    /// <summary>
+    /// 资源计数释放+请求打断管理器
+    /// </summary>
+    private ResourceScope mResourceScope;
+
+    public GameSceneManager()
+    {
+        mResourceScope = new ResourceScope();
+    }
 
     /// <summary>
     /// 初始化
@@ -39,30 +49,32 @@ public class GameSceneManager : SingletonTemplate<GameSceneManager>
     /// <param name="sceneName"></param>
     public void LoadSceneSync(string sceneName)
     {
-        // 场景资源计数采用手动管理计数的方式
-        // 切场景时手动计数减1
-        // 加载时手动计数加1，不绑定对象
-        // 减掉场景计数后，切换场景完成后再强制卸载所有不再使用的正常加载的Unsed资源(递归判定释放)
-        if (mCurrentSceneBundleLoader != null)
-        {
-            mCurrentSceneBundleLoader.ReleaseAssetBundle();
-            mCurrentSceneBundleLoader = null;
-        }
-
         BundleLoader bundleLoader;
         // 场景Asset比较特别，不是作为Asset加载，所以这里只加载所在AssetBundle
-        ResourceModuleManager.Singleton.RequstAssetABSync(
+        var assetBundleRequestHandle = ResourceModuleManager.Singleton.RequstAssetABSync(
         sceneName,
         out bundleLoader,
-        (Action<BundleLoader, AssetBundleRequestHandle>)((loader, requestHandle) =>
+        (Action<BundleLoader, AssetBundleRequestHandle>)((loader, assetBundleRequestHandle) =>
         {
-            mCurrentSceneBundleLoader = loader;
+            mResourceScope.RemoveRequest(assetBundleRequestHandle);
+            if(loader == null || !assetBundleRequestHandle.IsComplete)
+            {
+                return;
+            }
+            // 场景资源计数采用手动管理计数的方式
+            // 切场景时手动计数减1
+            // 加载时手动计数加1，不绑定对象
+            // 减掉场景计数后，切换场景完成后再强制卸载所有不再使用的正常加载的Unsed资源(递归判定释放)
+            ReleaseCurrentSceneRes();
+            // 场景的计数是加载所在AB上的
+            mCurrentSceneABPath = loader.ResourcePath;
             // 非AB模式会返回null
-            mCurrentSceneBundleLoader?.RetainAssetBundle();
+            mResourceScope.GetAssetBundle(loader);
             sceneName = Path.GetFileNameWithoutExtension(sceneName);
-            SceneManager.LoadScene(sceneName);
+            SceneManager.LoadSceneAsync(sceneName);
         }),
         ResourceLoadType.NormalLoad);
+        mResourceScope.RecordRequest(assetBundleRequestHandle);
     }
 
     /// <summary>
@@ -73,30 +85,44 @@ public class GameSceneManager : SingletonTemplate<GameSceneManager>
     /// <param name="sceneName"></param>
     public void LoadSceneAsync(string sceneName)
     {
-        // 场景资源计数采用手动管理计数的方式
-        // 切场景时手动计数减1
-        // 加载时手动计数加1，不绑定对象
-        // 减掉场景计数后，切换场景完成后再强制卸载所有不再使用的正常加载的Unsed资源(递归判定释放)
-        if (mCurrentSceneBundleLoader != null)
-        {
-            mCurrentSceneBundleLoader.ReleaseAssetBundle();
-            mCurrentSceneBundleLoader = null;
-        }
-
         BundleLoader bundleLoader;
         // 场景Asset比较特别，不是作为Asset加载，所以这里只加载所在AssetBundle
-        ResourceModuleManager.Singleton.RequstAssetABAsync(
+        var assetBundleRequestHandle = ResourceModuleManager.Singleton.RequstAssetABAsync(
         sceneName,
         out bundleLoader,
-        (Action<BundleLoader, AssetBundleRequestHandle>)((loader, requestHandle) =>
+        (Action<BundleLoader, AssetBundleRequestHandle>)((loader, assetBundleRequestHandle) =>
         {
-            mCurrentSceneBundleLoader = loader;
+            mResourceScope.RemoveRequest(assetBundleRequestHandle);
+            if(loader == null || !assetBundleRequestHandle.IsComplete)
+            {
+                return;
+            }
+            // 场景资源计数采用手动管理计数的方式
+            // 切场景时手动计数减1
+            // 加载时手动计数加1，不绑定对象
+            // 减掉场景计数后，切换场景完成后再强制卸载所有不再使用的正常加载的Unsed资源(递归判定释放)
+            ReleaseCurrentSceneRes();
+            // 场景的计数是加载所在AB上的
+            mCurrentSceneABPath = loader.ResourcePath;
             // 非AB模式会返回null
-            mCurrentSceneBundleLoader?.RetainAssetBundle();
+            mResourceScope.GetAssetBundle(loader);
             sceneName = Path.GetFileNameWithoutExtension(sceneName);
             SceneManager.LoadSceneAsync(sceneName);
         }),
         ResourceLoadType.NormalLoad);
+        mResourceScope.RecordRequest(assetBundleRequestHandle);
+    }
+
+    /// <summary>
+    /// 释放当前场景资源
+    /// </summary>
+    private void ReleaseCurrentSceneRes()
+    {
+        if (!string.IsNullOrEmpty(mCurrentSceneABPath))
+        {
+            mResourceScope.ReleaseResource(mCurrentSceneABPath);
+            mCurrentSceneABPath = null;
+        }
     }
 
     /// <summary>
